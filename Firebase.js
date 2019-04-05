c654468481cb79f42732bee27bc4d2f180c7b707
sortiz0997 <sortiz97@outlook.com>
2019-04-05 10:43:34 -0400

    modifications for export

--- Firebase.js
+++ Firebase.js
@@ -145,21 +145,21 @@ function addDevice(){
   document.getElementById('addNotification').innerHTML = "Added device";
 }
 // Code adapted from https://www.codexworld.com/export-html-table-data-to-excel-using-javascript/
-function exportTableToExcel(filename = ''){
+function exportTableToExcel(tableID, filename = ''){
+  view();
   var downloadLink;
   var dataType = 'application/vnd.ms-excel';
-  var tableSelect = document.getElementById('inventory');
-  view();
+  var tableSelect = document.getElementById(tableID);
   var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-
+  
   // Specify file name
   filename = filename?filename+'.xls':'excel_data.xls';
-
+  
   // Create download link element
   downloadLink = document.createElement("a");
-
+  
   document.body.appendChild(downloadLink);
-
+  
   if(navigator.msSaveOrOpenBlob){
       var blob = new Blob(['\ufeff', tableHTML], {
           type: dataType
@@ -168,10 +168,10 @@ function exportTableToExcel(filename = ''){
   }else{
       // Create a link to the file
       downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-
+  
       // Setting the file name
       downloadLink.download = filename;
-
+      
       //triggering the function
       downloadLink.click();
   }
