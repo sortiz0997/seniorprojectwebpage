0da2189b959af8837f8483c686713030eee83892
sortiz0997 <sortiz97@outlook.com>
2019-04-05 15:06:02 -0400

    modifications for export

--- Firebase.js
+++ Firebase.js
@@ -145,14 +145,12 @@ function addDevice(){
   document.getElementById('addNotification').innerHTML = "Added device";
 }
 // Code adapted from https://www.codexworld.com/export-html-table-data-to-excel-using-javascript/
-function exportTableToExcel(table, name, filename){
+function exportTableToExcel(tableID, filename = ''){
   view();
-  /*var downloadLink;
+  var downloadLink;
   var dataType = 'application/vnd.ms-excel';
   var tableSelect = document.getElementById(tableID);
   var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-  window.open('data:application/vnd.ms-excel,' + tableSelect);
-   
   // Specify file name
   filename = filename?filename+'.xls':'excel_data.xls';
   
@@ -175,19 +173,7 @@ function exportTableToExcel(table, name, filename){
       
       //triggering the function
       downloadLink.click(); 
-  }*/
-    let uri = 'data:application/vnd.ms-excel;base64,', 
-    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><title></title><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>', 
-    base64 = function(s) { return window.btoa(decodeURIComponent(encodeURIComponent(s))) },         format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; })}
-    
-    if (!table.nodeType) table = document.getElementById(table)
-    var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-
-    var link = document.createElement('a');
-    link.download = filename;
-    link.href = uri + base64(format(template, ctx));
-    link.click();
-
+  }
 }
 
 function editDevice(found){
